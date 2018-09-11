de4e4c8cdd184b6b60fcf62ef427263b4559ab40
Oli Evans <oli@tableflip.io>
2018-09-11 16:20:53 +0100

    feat: add tachyons flavour for comparison
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+
+import 'tachyons'
+import 'ipfs-css'
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.stories.js$/);
--- src/footer/footer.stories.js
+++ src/footer/footer.stories.js
@@ -2,12 +2,18 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import i18n from '../i18n'
 import themeDecorator from '../theme-decorator'
-import Footer from './'
+import StyledFooter from './footer.styled.js'
+import TachyonsFooter from './footer.tachyons.js'
 
 storiesOf('Footer', module)
   .addDecorator(themeDecorator)
-  .add('default', () => (
-    <div style={{fontFamily: 'system-ui' }}>
-      <Footer codeUrl="https://github.com/ipfs-shipyard/ipld-explorer-components" i18n={i18n} />
+  .add('styled-system', () => (
+    <div style={{fontFamily: 'system-ui', padding: 10 }}>
+      <StyledFooter codeUrl="https://github.com/ipfs-shipyard/ipld-explorer-components" i18n={i18n} />
+    </div>
+  ))
+  .add('tachyons', () => (
+    <div style={{fontFamily: 'system-ui', padding: 10 }}>
+      <TachyonsFooter codeUrl="https://github.com/ipfs-shipyard/ipld-explorer-components" i18n={i18n} />
     </div>
   ))
